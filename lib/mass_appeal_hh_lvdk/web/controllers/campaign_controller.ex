0307ce472fa0f49e5ba01ec02a228d0208fbc320
Liz Vanderkloot <liz@lizvdk.com>
2017-04-27 12:43:03 -0400

    User can create a new Campaign

--- lib/mass_appeal_hh_lvdk/web/controllers/campaign_controller.ex
+++ lib/mass_appeal_hh_lvdk/web/controllers/campaign_controller.ex
@@ -3,6 +3,8 @@ defmodule MassAppealHhLvdk.Web.CampaignController do
 
   alias MassAppealHhLvdk.Funding
 
+  action_fallback MassAppealHhLvdk.Web.FallbackController
+
   def index(conn, _params) do
     campaigns = Funding.list_campaigns()
     render(conn, "index.json", campaigns: campaigns)
@@ -11,5 +13,13 @@ defmodule MassAppealHhLvdk.Web.CampaignController do
   def show(conn, params) do
     campaign = Funding.get_campaign!(params["id"])
     render(conn, "show.json", campaign: campaign)
+  end
+
+  def create(conn, params) do
+    with {:ok, campaign} <- Funding.create_campaign(params["campaign"])
+      do conn
+        |> put_status(:created)
+        |> render("show.json", campaign: campaign)
+    end
   end
 end
