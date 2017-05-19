2bee5b2cfb4cedeab5d57f4eea3eec81905defe7
Liz Vanderkloot <liz@lizvdk.com>
2017-05-19 15:06:34 -0400

    Associate User hm Pledges

--- lib/mass_appeal_hh_lvdk/account/user.ex
+++ lib/mass_appeal_hh_lvdk/account/user.ex
@@ -1,11 +1,15 @@
 defmodule MassAppealHhLvdk.Account.User do
   use Ecto.Schema
 
+  alias MassAppealHhLvdk.Funding.Pledge
+
   schema "account_users" do
     field :email, :string
     field :first_name, :string
     field :last_name, :string
 
     timestamps()
+
+    has_many :funding_pledges, Pledge
   end
 end
